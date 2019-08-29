ea255d04499652171f1b6282e0313496a4dc12c5
huangshuisheng <huangss@silvrr.com>
2019-08-29 08:06:10 +0800

    log

--- app/src/main/java/com/xue/viewpagerdemo/basequick/InnerAdapter.java
+++ app/src/main/java/com/xue/viewpagerdemo/basequick/InnerAdapter.java
@@ -2,6 +2,8 @@ package com.xue.viewpagerdemo.basequick;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -16,5 +18,12 @@ public class InnerAdapter extends BaseQuickAdapter<String,BaseViewHolder> {
     @Override
     protected void convert(BaseViewHolder helper, String item) {
         helper.setText(R.id.textview,item);
+        Log.e("InnerAdapter","convert:"+item);
+    }
+
+    @Override
+    protected BaseViewHolder onCreateDefViewHolder(ViewGroup parent, int viewType) {
+        Log.e("InnerAdapter","onCreateDefViewHolder");
+        return super.onCreateDefViewHolder(parent, viewType);
     }
 }
